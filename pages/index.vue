2dba9fe2ecf5a20d61e87199b3b02cd692561824
mwakosy-julius <mwakosyajulius619@gmail.com>
2022-03-30 14:11:22 +0300

    karanga

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
       <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
           <h1 class="display-4 fw-normal">New Genomi</h1>
-          <p class="lead fw-normal">Best Place for Searching Gene and Protein Sequences</p>
+          <p class="lead fw-normal">Bioinformatics tool for Searching Gene and Protein Sequences</p>
           <nuxt-link class="btn btn-outline-secondary" to="./gene">Let's Get Started</nuxt-link>
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
